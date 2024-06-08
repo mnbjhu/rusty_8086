dd0cfe2e1df03a66802f7dca73ff87eab85cdc4c
James Gibson <james-gibson@ntlworld.com>
2024-06-08 20:08:27 +0100

    Fix syntax error

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use clap::Parser;
 
 use crate::cli::Command;
 
-mod decoder;
-#![cfg(not(tarpaulin_include))]
+#[cfg(not(tarpaulin_include))]
 mod cli;
+mod decoder;
 
 fn main() {
     Command::parse().run()
